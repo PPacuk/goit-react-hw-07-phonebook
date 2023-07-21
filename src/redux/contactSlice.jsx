91d0c9242d1d34dfd8dfc4d6072f4a8d8f119658
Piotr Pacuk <piotrek.pacuk@gmail.com>
2023-07-21 12:33:31 +0200

    Update contactSlice.jsx
    added delete notifi

--- src/redux/contactSlice.jsx
+++ src/redux/contactSlice.jsx
@@ -1,7 +1,13 @@
 import { nanoid } from 'nanoid';
 import { createSlice } from '@reduxjs/toolkit';
+import { Notify } from 'notiflix';
 
-const contactInitialState = [];
+const contactInitialState = [
+  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+];
 
 export const contactSlice = createSlice({
   name: 'contacts',
@@ -25,6 +31,7 @@ export const contactSlice = createSlice({
     deleteContact(state, action) {
       const index = state.findIndex(contact => contact.id === action.payload);
       state.splice(index, 1);
+      Notify.success(`Contact removed from list!`)
     },
   },
 });
