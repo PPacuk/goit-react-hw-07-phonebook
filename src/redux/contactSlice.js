9896d64aaae7ce84bbe5977151e66684c645bf8f
Piotr Pacuk <piotrek.pacuk@gmail.com>
2023-07-21 17:49:38 +0200

    Update contactSlice.js

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -42,7 +42,7 @@ const contactSlice = createSlice({
       state.loading = false;
       state.error = null;
       const index = state.items.findIndex(
-        contact => contact.id === action.payload.id
+        ({id}) => id === action.payload.id
       );
       state.items.splice(index, 1);
       Notify.success(`Contact removed from list!`);
